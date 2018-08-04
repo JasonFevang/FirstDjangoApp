a02f8696015a3a5d25171e8c4639c1d88653a5fb
Jason Fevang <jasonfevang@gmail.com>
2018-08-04 00:22:22 -0700

    root index page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
-# from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
-from .models import Question, Choice
+from .models import Question
 
 # Create your views here.
 
